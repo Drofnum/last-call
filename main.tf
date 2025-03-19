bad465729813f528574863c8185fae1b9fb9dabf
aj-munford_epicor <echaosaj@gmail.com>
2025-03-19 13:09:33 -0600

    fix incorrect references

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_s3_bucket_public_access_block" "web_bucket_public_access" {
 
 # Upload website content to S3
 resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.id
+  bucket = aws_s3_bucket.web_bucket.id
   key    = "index.html"
   source = "web/index.html"
   etag   = filemd5("web/index.html")
@@ -154,7 +154,7 @@ resource "aws_s3_bucket_policy" "web_bucket_policy" {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name              = aws_s3_bucket.web_bucket.bucket_regional_domain_name
-    origin_id                = local.s3_origin
+    origin_id                = local.s3_origin_id
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
 
